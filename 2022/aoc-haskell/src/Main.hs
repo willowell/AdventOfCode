1ed51d32468bbda4ca59b81de3aa5a2de46b46ea
William Howell <wlm.howell@gmail.com>
2022-12-02 09:32:44 -0500

    (2022/hs) hook up Day 2 solution to main

--- 2022/aoc-haskell/src/Main.hs
+++ 2022/aoc-haskell/src/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import AOC.Harness
 import qualified Day01
+import qualified Day02
 
 solutions :: Solutions
 solutions = solutionsFromList
   [
     S Day01.solution
+  , S Day02.solution
   ]
 
 main :: IO ()
