78977f46d34e5707a7b6887e000a87db84403c0d
William Howell <wlm.howell@gmail.com>
2022-12-05 14:05:52 -0500

    (2022/hs) day 5 part a now works on test input but not on live input

--- 2022/aoc-haskell/solutions/Day05.hs
+++ 2022/aoc-haskell/solutions/Day05.hs
@@ -6,33 +6,92 @@ import Data.List
 import Data.Maybe
 import Util
 
+import qualified Data.Vector.Mutable  as MVec
+
 type Instruction = (Int, Int, Int)
 
 data Procedure = Procedure { stacks :: [[Char]], instructions :: [Instruction] }
 
-instance Show Procedure where
-  show a = (show $ stacks a) <> (show $ instructions a)
+prettyPrintProcedure p =
+  "Stacks: `" <> (show $ stacks p) <> "`\nInstructions: `" <> (show $ instructions p) <> "`."
+
+replaceStack :: [[Char]] -> [Char] -> [Char] -> [[Char]]
+replaceStack stacks stack newStack = map (\s -> if s == stack then newStack else s) stacks
+
+rearrange :: [[Char]] -> Int -> Int -> Int -> [[Char]]
+rearrange stacks numCrates from to =
+  let fromStack = stacks !! (from - 1) in
+  let toStack = stacks !! (to - 1) in
+
+  let movedCrates = take numCrates fromStack in
+
+  let oldStack = drop numCrates fromStack in
+
+  let newStack = reverse movedCrates <> toStack in
+
+  let stacksWithReplacedTo = replaceStack stacks toStack newStack in
+    replaceStack stacksWithReplacedTo fromStack oldStack
+
+getTopCrates :: [[Char]] -> [Char]
+getTopCrates stacks = map head stacks
+
+runInstructions :: Procedure -> [[Char]]
+runInstructions p =
+  let ins = instructions p in
+  let startingStacks = stacks p in
+    foldl
+    (\curStacks (numCrates, from, to) -> rearrange curStacks numCrates from to )
+    startingStacks
+    ins
+
+runInstructions' :: Foldable t => [[Char]] -> t (Int, Int, Int) -> [[Char]]
+runInstructions' stacks instructions =
+  let ins = instructions in
+  let startingStacks = stacks in
+    foldl
+    (\curStacks (numCrates, from, to) -> rearrange curStacks numCrates from to)
+    startingStacks
+    ins
+
 
 solution :: Solution
 {- Parser Output Type -} Procedure
-{- Part A Output Type -} Procedure
-{- Part B Output Type -} [[Char]]
+{- Part A Output Type -} String
+{- Part B Output Type -} String
 solution = Solution
   {
       {- | Input Parser
+
+      For the test input, returns stacks "ZN,MCD,P"
+      
+      For ease of use, we can reverse the stacks:
+      1. NZ
+      2. DCM
+      3. P
+
+      Next step:
+      1. DNZ
+      2. CM
+      3. P
+
+      Next step:
+      1. <empty>
+      2. CM
+      3. ZNDP
       -}
       decodeInput = pProcedure
     , solveA = defSolver
       {
         {- | Part A Solver
         -}
-        solve = Just . id
+        solve = Just <$> getTopCrates . runInstructions
+
       }
     , solveB = defSolver
       {
         {- | Part B Solver
         -}
-        solve = pure Nothing
+        solve = Just . prettyPrintProcedure
       }
     , tests =
       [
@@ -46,7 +105,7 @@ solution = Solution
           , "move 3 from 1 to 3"
           , "move 2 from 2 to 1"
           , "move 1 from 1 to 2"
-        ] :=> [(PartA, "CMZ"), (PartB, "")]
+        ] :=> [(PartA, "\"CMZ\""), (PartB, "\"foo\"")]
       ]
   }
   where
@@ -78,9 +137,9 @@ solution = Solution
 
     pProcedure :: Parser Procedure
     pProcedure = do
-      maybeStacks <- dbg "stacks" (someTill (dbg "stack" pStack <* eol) pNumberLabels) <?> "stacks"
+      maybeStacks <- (someTill (pStack <* eol) pNumberLabels) <?> "stacks"
 
-      let stacks = map catMaybes $ transpose . reverse $ maybeStacks
+      let stacks = map reverse $ map catMaybes $ transpose . reverse $ maybeStacks
 
       void eol <?> "empty line"
 
